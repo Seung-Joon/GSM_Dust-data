9da5f10c9cb8947e95154a3b661b7a417eb8f7f1
GuGU <wnduddl3409@naver.com>
2019-04-29 16:02:04 +0900

    Update SerialTimeOut.py

--- SerialTimeOut.py
+++ SerialTimeOut.py
@@ -10,11 +10,11 @@ class DataManager():
         self.url = 'https://gsm-dustdata.firebaseio.com'
         self.fbase = firebase.FirebaseApplication(self.url, None)
         self.now = time.localtime()
-        self.key = "%04d%02d%02d%02d%02d%02d" % (self.now.tm_year, self.now.tm_mon, self.now.tm_mday, self.now.tm_hour, self.now.tm_min, self.now.tm_sec)
-    
+        self.key = "%04d%02d%02d%02d%02d%02d" % tuple(tm for tm in self.now[:6])
+
     @contextmanager
     def timeout(self, time):
-        signal.signal(signal.SIGALRM, raise_timeout)
+        signal.signal(signal.SIGALRM, self.raise_timeout)
         signal.alarm(time)
         try:
             yield
@@ -45,17 +45,16 @@ class DataManager():
                     return dataFrame 
             except:
                 print("Fail to request")
-                err_li = [int(-1)]
-                return err_li
+                return [int(-1)]
 
     def dataDecode(self, data_list):
-        if len(data_list) > 1:
-            return {'PM1.0': data_list[2], 'PM2.5' : data_list[3],
-                    'PM10' : data_list[4], 'V_SYSTEM' : data_list[6] / data_list[7],
-                    'V_SOLAR' : data_list[9] / data_list[10], 'TEMPURATURE' : data_list[11],
-                    'HUMIDITY' : data_list[12], 'ERR' : 0}
-        return {'ERR' : -1}
-
+        if len(data_list) < 2:
+            return {'ERR' : -1}
+        return {'PM1.0': data_list[2], 'PM2.5' : data_list[3],
+                'PM10' : data_list[4], 'V_SYSTEM' : data_list[6] / data_list[7],
+                'V_SOLAR' : data_list[9] / data_list[10], 'TEMPURATURE' : data_list[11],
+                'HUMIDITY' : data_list[12], 'ERR' : 0}
+        
     def dataSynchronization(self, dataFrame): 
         try:
             self.fbase.put('/','data', self.dataDecode(dataFrame))
@@ -77,7 +76,5 @@ if __name__ == "__main__":
         print(SYS_DATA_FRAME)
         print('updating data')
         DM.dataSynchronization(SYS_DATA_FRAME)
-        DM.dataAccumulation(str(DM.getDateValue()), SYS_DATA_FRAME) 
+        DM.dataAccumulation(str(DM.getDateValue()), SYS_DATA_FRAME)
         print('done..')
-        
-
