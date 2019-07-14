5628a9ccc6802961b4f0c65de5a8f99dc38529e1
GuGU <wnduddl3409@naver.com>
2019-07-14 18:28:01 +0900

    Update GetData.py

--- GetData.py
+++ GetData.py
@@ -5,7 +5,5 @@ url = "https://gsm-dustdata-2f7b8.firebaseio.com"
 fbase = firebase.FirebaseApplication(url, None)
 datas = fbase.get("/AccData", None)
 
-for data in datas:
-  print(datas[data])
-
-  
+with open('DustData.json', 'w') as json_file:
+  json.dump(datas, json_file)
